aaa3653254281fcbf9a0836260cb94f4ece6dcd8
mhutchinson11 <98057736+mhutchinson11@users.noreply.github.com>
2022-04-22 10:24:26 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner Readability = new Scanner (System.in);
+            System.out.println("Text: ");
+            String text = Readability.nextLine();
+
+            int countletters = 0;
+            int countwords = 1;
+
+
+            for (int i = 0; i < text.length(); i++) {
+                if (Character.isLetter(text.charAt(i)))
+                    countletters++;}
+
+            for (int i = 0; i < text.length() - 1; i++) {
+                if ((text.charAt(i) == ' ') && (text.charAt(i + 1) != ' ')) {
+                    countwords++;
+                }
+            }
+            int countsentences = text.split("[!?.:]+").length;
+
+            System.out.println(countletters + " Letters");
+            System.out.println(countsentences + " Sentences");
+            System.out.println(countwords + " Words");
+
+
+            float L = 1.0f * countletters / countwords * 100;
+            float S = 1.0f * countsentences / countwords * 100;
+
+            int grade = (int) Math.round(0.0588 * L - 0.296 * S - 15.8);
+
+            if (grade < 1) {
+                System.out.println("Before Grade 1");
+            }
+            else if (grade >= 16)
+                System.out.println("Grade 16+");
+            else
+                System.out.println("Grade: " + grade);    }
+}
